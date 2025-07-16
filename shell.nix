914b081ac858f82608c2b16d36f878e57b407705
Pierre-Yves Landuré <pierre-yves@landure.fr>
2025-07-16 09:31:05 +0200

    style(devenv): 🎨 format code for better readability and maintainability

--- shell.nix
+++ shell.nix
@@ -1,10 +1,14 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
-  pkgs = import nixpkgs { config = {}; overlays = []; };
+  pkgs = import nixpkgs {
+    config = { };
+    overlays = [ ];
+  };
 in
 
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
     devenv
+    direnv
   ];
 }
